129ecf87573bf17abcea883ba108fefe3bb90763
Sebas04pm <oscartelegram123@gmail.com>
2023-06-27 01:43:27 -0400

    Rutas, tailwind y secciones

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,12 @@
-
+import Navbar from "../src/components/Navbar";
+import Inicio from "../src/components/Inicio";
+import Formulario from "../src/components/Formulario";
+import Footer from "../src/components/Footer";
+import Planes from "./components/Planes";
+import PlanesVistaPrevia from "./components/PlanesVistaPrevia";
+import Experiencias from "../src/components/Experiencias";
+import Nosotros from "../src/components/Nosotros";
+import NotFound from "../src/components/Pagina404";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
